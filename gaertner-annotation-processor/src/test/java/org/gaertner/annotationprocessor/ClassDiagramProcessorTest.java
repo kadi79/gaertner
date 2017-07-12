6b046fe48237dfeaab5d93419b9c36d99283cfe6
Dirk Kaspar <dirk-kaspar@users.noreply.github.com>
2017-07-12 05:35:57 +0200

    in test compile result to temp dir and delete afterwards

--- gaertner-annotation-processor/src/test/java/org/gaertner/annotationprocessor/ClassDiagramProcessorTest.java
+++ gaertner-annotation-processor/src/test/java/org/gaertner/annotationprocessor/ClassDiagramProcessorTest.java
@@ -1,56 +1,32 @@
 package org.gaertner.annotationprocessor;
 
-import static org.junit.Assert.*;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.StringWriter;
 import java.io.Writer;
-import java.nio.charset.Charset;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileAttribute;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Locale;
 
 import javax.tools.JavaCompiler;
 import javax.tools.JavaCompiler.CompilationTask;
+import javax.tools.JavaFileManager.Location;
 import javax.tools.JavaFileObject;
-import javax.tools.JavaFileObject.Kind;
 import javax.tools.StandardJavaFileManager;
 import javax.tools.StandardLocation;
 import javax.tools.ToolProvider;
 
-import org.easymock.EasyMock;
-import org.easymock.IAnswer;
-import org.gaertner.annotationprocessor.puml.model.DiagramFactory;
-import org.gaertner.annotationprocessor.puml.model.classdiagram.ClassDiagram;
 import org.gaertner.annotationprocessor.util.TeeWriter;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class ClassDiagramProcessorTest {
 
-	@Ignore
-	@Test
-	public void runAnnotationProcessor() throws Exception {
-		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-		StringWriter stringWriter = new StringWriter();
-		OutputStreamWriter outputStreamWriter = new OutputStreamWriter(System.out, "UTF-8");
-		Writer writer = new TeeWriter(outputStreamWriter, stringWriter);
-		Iterable<? extends JavaFileObject> classFiles = getClassFiles("src/test/emptyClassTest");
-		StandardJavaFileManager standardFileManager = compiler.getStandardFileManager(null, null, null);
-		CompilationTask task = compiler.getTask(writer, null, null, null, null, classFiles);
-		task.setProcessors(Arrays.asList(new ClassDiagramProcessor()));
-		task.call();
-	}
-
-	private Iterable<? extends JavaFileObject> getClassFiles(String sourcePath) throws IOException {
-		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-		StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, Locale.GERMANY, Charset.forName("utf-8"));
-		fileManager.setLocation(StandardLocation.SOURCE_PATH, Arrays.asList(new File(sourcePath)));
-		return fileManager.list(StandardLocation.SOURCE_PATH, "", Collections.singleton(Kind.SOURCE), true);
-	}
-	
 	@Test
 	public void testName() throws Exception {
 		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
@@ -68,9 +44,32 @@ public class ClassDiagramProcessorTest {
 				"\n" + 
 				"}\n" + 
 				""));
-		StandardJavaFileManager standardFileManager = compiler.getStandardFileManager(null, null, null);
-		CompilationTask task = compiler.getTask(writer, null, null, null, null, classFiles);
+		StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
+		Path targetDir = Files.createTempDirectory("target");
+		File classTarget = new File(targetDir.toFile(), "classes");
+		Files.createDirectory(classTarget.toPath());
+		File generatedSourcesTarget = new File(targetDir.toFile(), "generatedSources");
+		Files.createDirectory(generatedSourcesTarget.toPath());
+		fileManager.setLocation(StandardLocation.CLASS_OUTPUT, Arrays.asList(classTarget));
+		fileManager.setLocation(StandardLocation.SOURCE_OUTPUT, Arrays.asList(generatedSourcesTarget));
+		CompilationTask task = compiler.getTask(writer, fileManager, null, null, null, classFiles);
 		task.setProcessors(Arrays.asList(new ClassDiagramProcessor()));
 		task.call();
+		
+		Files.walkFileTree(targetDir, new SimpleFileVisitor<Path>() {
+
+			@Override
+			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+				Files.delete(file);
+				return FileVisitResult.CONTINUE;
+			}
+
+			@Override
+			public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+				if (exc != null) throw exc;
+				Files.delete(dir);
+				return FileVisitResult.CONTINUE;
+			}
+		});
 	}
 }
